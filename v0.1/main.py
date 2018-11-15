97db708e158c25b9c59eb5e7ed58b68b39c92987
annamiller <amiller@physics.ucsd.edu>
2018-11-15 15:32:32 -0800

    Updated README, and main.py file to automatically import jason's version of lab manager if running on python 27

--- v0.1/main.py
+++ v0.1/main.py
@@ -5,15 +5,17 @@ from __future__ import unicode_literals
 import sys
 if sys.version_info.major > 2:
     xrange = range
+    import lab_manager as lm
+
 elif sys.version_info.major == 2:
-    pass
+    import lab_manager_py27 as lm
 # end boiler plate for compatibility
 
 import numpy as np
 import networks
 import neuron_models as nm
 import experiments
-import lab_manager
+
 #from importlib import reload  # Python 3.4+ only.
 # reload(networks)
 # reload(experiments)
@@ -36,16 +38,16 @@ experiments.feed_gaussian_rate_poisson_spikes(
     time_per_sniff=time_per_sniff)
 
 # step 3: ask our lab manager to set up the lab for the experiment.
-f, initial_conditions, _ = lab_manager.set_up_lab(net)
+f, initial_conditions, _ = lm.set_up_lab(net)
 
 # step 4: run the lab and gather data
 time_sampled_range = np.arange(0., total_time, 0.1)
-data = lab_manager.run_lab(f, initial_conditions, time_sampled_range)
+data = lm.run_lab(f, initial_conditions, time_sampled_range)
 
 # Time to witness some magic
 #lab_manager.sample_plot(time_sampled_range, data, net)
 for layer_idx in range(len(net.layers)):
-    lab_manager.show_all_neuron_in_layer(
+    lm.show_all_neuron_in_layer(
         time_sampled_range, data, net, layer_idx)
-    lab_manager.show_all_synaspe_onto_layer(
+    lm.show_all_synaspe_onto_layer(
         time_sampled_range, data, net, layer_idx)
