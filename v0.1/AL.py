7b722b273a5cd3711d11ca9145acb00849d2f109
annamiller <amiller@physics.ucsd.edu>
2018-11-17 10:02:25 -0800

    Fixed AL

--- v0.1/AL.py
+++ v0.1/AL.py
@@ -25,15 +25,16 @@ import experiments as ex
 from itertools import chain
 
 #First number is #LNs, second is #PNs
-neuron_nums = [10, 30] # 2 LNs and 6PNs
+neuron_nums = [2, 6]
 #Create_AL creates AL with random connections with prob 0.5
 AL = net.create_AL(nm.LN, nm.PN2, nm.Synapse_gaba_LN, nm.Synapse_nAch_PN2, neuron_nums)
-#AL = net.create_AL_man(nm.LN,nm.PN2,nm.Synapse_gaba_LN, nm.Synapse_nAch_PN2)
+
 #Set up the experiment
 num_layers = 2
 
+# Roughly the max current each neuron is injected with. This block of code makes it
+# so that random neurons are injected with current, and the levels are slighlty different
 Iscale = 300
-
 I_ext = []
 for i in range(num_layers):
     I_ext.append(np.random.rand(neuron_nums[i]))
@@ -43,9 +44,7 @@ for i in range(num_layers):
 neuron_inds = [np.nonzero(I_ext[j])[0].tolist() for j in range(num_layers)]
 current_vals = [I_ext[j][np.nonzero(I_ext[j])] for j in range(num_layers)]
 
-#neuron_inds = [[0,1], [1,3]]
-#current_vals = [[400, 400], [300, 300]]
-#ex.constant_current_on_top_layer(AL,300)
+
 ex.const_current(AL, num_layers, neuron_inds, current_vals)
 #ex.feed_gaussian_rate_poisson_spikes(AL,0.1,i_max=1000,num_sniffs=50)
 #set up the lab
@@ -58,5 +57,5 @@ time_sampled_range = np.arange(0., time_len, dt)
 
 data = lm.run_lab(f, initial_conditions, time_sampled_range, integrator = 'dopri5')
 
-lm.show_all_neuron_in_layer(time_sampled_range, data, AL, 0)
+#lm.show_all_neuron_in_layer(time_sampled_range, data, AL, 0)
 lm.plot_LFP(time_sampled_range, data, AL, layer_pn = 1)
